e15cacc3330e6ec4fffb3f2b476a37bf964e8ff2
Nikita <121382716+Dovolentoboy@users.noreply.github.com>
2023-02-26 20:20:37 +0300

    Update Xp_system.py

--- Xp_system.py
+++ Xp_system.py
@@ -2,7 +2,7 @@ import disnake
 from disnake.ext import commands
 from pymongo import MongoClient
 
-cluster = MongoClient(LINK)
+cluster = MongoClient('mongodb+srv://Dovolentoboy:a600370@cluster0.6wxtw41.mongodb.net/test')
 db = cluster['ecodb']
 collection = db['users']
 
@@ -16,11 +16,63 @@ class Xp_system (commands.Cog):
         if member is None:
             embed = disnake.Embed(
                 title='Ваш опыт',
+                description=f'```{collection.find_one({"_id":ctx.author.id})["xp"]}```\n'
+                f'Ваш уровень\n'
+                f'```{collection.find_one({"_id":ctx.author.id})["lvl"]}```'
+            )
+            await ctx.send(embed=embed)
+        else:
+            embed = disnake.Embed(
+                title='Ваш опыт',
                 description=f'```{collection.find_one({"_id":member.id})["xp"]}```\n'
                 f'Ваш уровень\n'
                 f'```{collection.find_one({"_id":member.id})["lvl"]}```'
             )
             await ctx.send(embed=embed)
+    
+    @commands.Cog.listener()
+    async def on_message(self,message):
+        if message.author == self.bot.user:
+            return
+        user = message.author
+        data = collection.find_one({'_id':user.id})
+        if data['xp'] == 1000 + 1000 * data['lvl']:
+            collection.update_one(
+                {
+                '_id':user.id
+                },
+                {
+                '$inc':{
+                    'lvl':1
+                }
+                }
+            )
+
+            collection.update_one(
+                {
+                '_id':user.id
+                },
+                {
+                '$set':{
+                    'xp':0
+                }
+                }
+            )
+            
+        else:
+            collection.update_one(
+                {
+                '_id':user.id
+                },
+                {
+                    '$inc':{
+                        'xp':5
+                    }
+                }
+            )
+
+
+    
 
 
 def setup(bot):
